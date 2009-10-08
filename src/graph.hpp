1da87c22390d648536a16e4fcd3ea4a57308a246
Felix H. Dahlke <fhd@ubercode.de>
2009-10-08 21:33:16 +0000

    Fixed wrong white spaces

--- src/graph.hpp
+++ src/graph.hpp
@@ -1,8 +1,8 @@
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
 
-#include <map>
 #include <deque>
+#include <map>
 
 /** Contains types for building and searching a graph */
 namespace graph
@@ -11,16 +11,16 @@ namespace graph
 	typedef unsigned int node_t;
 
 	/** Multiple nodes */
-	typedef std::deque <node_t> nodes_t;
+	typedef std::deque<node_t> nodes_t;
 
 	/** A path through a graph */
-	typedef std::deque <node_t> path_t;
+	typedef std::deque<node_t> path_t;
 
 	/** A graph **/
-	typedef std::map <node_t, nodes_t> graph_t;
+	typedef std::map<node_t, nodes_t> graph_t;
 
 	/** An agenda for a search algorithm */
-	typedef std::deque <path_t> agenda_t;
+	typedef std::deque<path_t> agenda_t;
 }
 
 #endif /* GRAPH_HPP */
